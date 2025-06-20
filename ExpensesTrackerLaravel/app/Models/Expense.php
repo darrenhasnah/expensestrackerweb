c95b19eded9c0408d22fd6d80b679d4ea06c932b
Darren Hasnah <159036668+darrenhasnah@users.noreply.github.com>
2025-06-20 14:03:22 +0700

    memindahkan project ke laravel, set up database

---
+++ ExpensesTrackerLaravel/app/Models/Expense.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'amount',
+        'category',
+        'description',
+        'date',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date' => 'date',
+            'amount' => 'decimal:2',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
